763367cf585b93999565d846f4aae5abe4bed6cb
A_footballfish <cat.3210sm@gmail.com>
2015-12-23 01:16:35 +0900

    ネットワークプロジェクトいれたよ

---
+++ NewNetworkProject (1)/Assets/Assets/Scripts/DisableRemotePlayerBehaviours.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class DisableRemotePlayerBehaviours : NetworkBehaviour {
+
+	public Behaviour[] behaviours;
+
+	// Use this for initialization
+	void Start () {
+		//登録されたコンポーネントをリモート側で disableにする
+		if (!isLocalPlayer) {
+			foreach (var behaviour in behaviours) {
+				behaviour.enabled = false;
+			}
+		}
+	}
+}
