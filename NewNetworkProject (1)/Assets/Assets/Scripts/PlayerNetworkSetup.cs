6c436e5240a2a847517d94cceb6e16e98d6862c0
A_footballfish <cat.3210sm@gmail.com>
2015-12-23 12:43:25 +0900

    animation関連いじってました

---
+++ NewNetworkProject (1)/Assets/Assets/Scripts/PlayerNetworkSetup.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerNetworkSetup : NetworkBehaviour {
+	
+	public override void OnStartLocalPlayer ()
+	{
+		//LocalPlayerのAnimatorパラメータを自動的に送る
+		GetComponent<NetworkAnimator>().SetParameterAutoSend(0, true);
+	}
+
+	public override void PreStartClient ()
+	{
+		//ClientのAnimatorパラメータを自動的に送る
+		GetComponent<NetworkAnimator>().SetParameterAutoSend(0, true);
+	}
+}
