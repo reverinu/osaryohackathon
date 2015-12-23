1d23e1683d7fb96b2b53f37d3730dd2e946dc60f
A_footballfish <cat.3210sm@gmail.com>
2015-12-23 14:31:11 +0900

    スムーズにできませんでした

---
+++ NewNetworkProject (1)/Assets/Assets/Scripts/Sync_playerPosition.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking; //UNETを使う時に必要なライブラリ
+
+//UNETを使う時に必要な継承
+public class Sync_playerPosition : NetworkBehaviour {
+
+	[SyncVar]	//ホストから全クライアントへ送られる
+	private Vector3 syncPos;
+	//Playerの現在位置
+	[SerializeField] Transform myTransform;
+	//Lerp: ２ベクトル間を補間する
+	[SerializeField] float lerpRate = 15;
+
+	void FixedUpdate ()
+	{
+		TransmitPosition(); 
+		LerpPosition();	//2点間を補間する
+	}
+
+	//ポジション補間用メソッド
+	void LerpPosition ()
+	{
+		//補間対象は相手プレイヤーのみ
+		if (!isLocalPlayer) {
+			//Lerp(from, to, 割合) from〜toのベクトル間を補間する
+			myTransform.position = Vector3.Lerp(myTransform.position, syncPos, Time.deltaTime * lerpRate);
+		}
+	}
+	//クライアントからホストへ、Position情報を送る
+	[Command]
+	void CmdProvidePositionToServer (Vector3 pos)
+	{
+		//サーバー側が受け取る値
+		syncPos = pos;
+	}
+
+	//クライアントのみ実行される
+	[ClientCallback]
+	//位置情報を送るメソッド
+	void TransmitPosition ()
+	{
+		if (isLocalPlayer) {
+			CmdProvidePositionToServer(myTransform.position);
+		}
+	}
+}
