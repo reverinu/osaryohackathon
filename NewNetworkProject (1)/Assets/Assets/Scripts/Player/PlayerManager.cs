a2b12e4be10658b60427d50474d29b124f0af4eb
A_footballfish <cat.3210sm@gmail.com>
2015-12-23 18:39:50 +0900

    ロビー概念導入。フォルダー作成 あわせてビルドセッティングのシーン順の変更を行った。

---
+++ NewNetworkProject (1)/Assets/Assets/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerManager : MonoBehaviour {
+
+	public  static int MenberNumber = 0;
+	private string[] name = new string[4];
+
+	// Use this for initialization
+	void Start () {
+		DontDestroyOnLoad(this.gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (MenberNumber);
+	}
+
+	//プレイヤー数の登録
+	public void PlayerCount()
+	{
+		MenberNumber++;
+	}
+
+	//鬼の割当(人数が増えるたびに行われる)
+	void AssignOgre()
+	{
+
+	}
+}
