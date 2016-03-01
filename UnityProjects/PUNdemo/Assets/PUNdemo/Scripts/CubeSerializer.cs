51610ea37638e5d189731a0cf82bba5f0f8e22d4
reverinu <reverinudog@yahoo.co.jp>
2016-03-01 21:25:00 +0900

    PUNdemoの作成

---
+++ UnityProjects/PUNdemo/Assets/PUNdemo/Scripts/CubeSerializer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeSerializer : Photon.MonoBehaviour
+{
+    void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            //データの送信
+            //stream.SendNext(transform.position);
+            //stream.SendNext(transform.rotation);
+            stream.SendNext(GetComponent<Renderer>().material.color.r);
+            stream.SendNext(GetComponent<Renderer>().material.color.g);
+            stream.SendNext(GetComponent<Renderer>().material.color.b);
+            stream.SendNext(GetComponent<Renderer>().material.color.a);
+        }
+        else
+        {
+            //データの受信
+            //transform.position = (Vector3)stream.ReceiveNext();
+            //transform.rotation = (Quaternion)stream.ReceiveNext();
+            float r = (float)stream.ReceiveNext();
+            float g = (float)stream.ReceiveNext();
+            float b = (float)stream.ReceiveNext();
+            float a = (float)stream.ReceiveNext();
+            GetComponent<Renderer>().material.color = new Vector4(r, g, b, a);
+        }
+    }
+}
---
+++ UnityProjects/PUNdemo/Assets/PUNdemo/Scripts/GameController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+
+    private PhotonView photonView;
+
+    void Start()
+    {
+        photonView = PhotonView.Get(this);
+    }
+
+    void Update()
+    {
+        if (photonView.isMine)
+        {
+            float x = Input.GetAxis("Horizontal");
+            float z = Input.GetAxis("Vertical");
+            transform.Translate(x * 0.2f, 0, z * 0.2f);
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                GetComponent<Renderer>().material.color = ChangeColor();
+            }
+        }
+    }
+
+    Color ChangeColor()
+    {
+        switch (Random.Range(0, 5))
+        {
+            case 0: return Color.red;
+            case 1: return Color.blue;
+            case 2: return Color.green;
+            case 3: return Color.yellow;
+            case 4: return Color.white;
+            case 5: return Color.black;
+        }
+        return Color.clear;
+    }
+}
